3f381c3943ead87a44c355fe6abf9b9e5189f6a3
unknown <zab_vl@tutby>
2023-04-11 19:24:49 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int M =int.Parse(Console.ReadLine());
+
+int Numbers()
+{
+  if(M<1)
+  {
+    return M;
+  }
+     Console.Write(M+" ");
+    M--;
+    return Numbers();
+}
+
+Numbers();
